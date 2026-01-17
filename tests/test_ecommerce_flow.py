f823a375acb33bc9e3fd712fdf2643737c7bfbbc
Patryk Przybysz <pprzybysz04@outlook.com>
2026-01-17 14:20:10 +0100

    setup selenium

--- tests/test_ecommerce_flow.py
+++ tests/test_ecommerce_flow.py
@@ -1,2 +1,17 @@
-def test_ecommerce_flow():
+import pytest
+
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+def test_ecommerce_flow(driver):
+    driver.get("https://www.google.com")
     assert 1 == 1

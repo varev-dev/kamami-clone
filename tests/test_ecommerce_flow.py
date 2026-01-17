fcc5760db461fa74748a9645ee760855927f895c
Patryk Przybysz <pprzybysz04@outlook.com>
2026-01-17 14:21:47 +0100

    setup webdriver-manager

--- tests/test_ecommerce_flow.py
+++ tests/test_ecommerce_flow.py
@@ -1,11 +1,15 @@
 import pytest
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    location = ChromeDriverManager().install()
+    service = Service(location)
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver

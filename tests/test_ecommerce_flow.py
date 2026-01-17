776cb3664fec8c76ab3e3d556627493bb19f98c1
Patryk Przybysz <pprzybysz04@outlook.com>
2026-01-17 14:41:21 +0100

    add configurable BASE_URL and language preference

--- tests/test_ecommerce_flow.py
+++ tests/test_ecommerce_flow.py
@@ -4,12 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from os import getenv
+
+BASE_URL = getenv("BASE_URL", "https://localhost:8443/")
+
 
 @pytest.fixture(scope="function")
 def driver():
     location = ChromeDriverManager().install()
     service = Service(location)
-    driver = webdriver.Chrome(service=service)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--accept-lang=pl")
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
@@ -17,5 +24,5 @@ def driver():
 
 
 def test_ecommerce_flow(driver):
-    driver.get("https://www.google.com")
-    assert 1 == 1
+    driver.get(BASE_URL)
+    assert driver.current_url.startswith(BASE_URL)
